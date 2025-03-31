5660b7e9655fda3d5c81634ae0d046bd041b7860
Charles BIOT <act.charlesBIOT@gmail.com>
2025-03-31 09:30:25 +0800

    i love u

--- Speeches/MainPage.xaml.cs
+++ Speeches/MainPage.xaml.cs
@@ -8,16 +8,19 @@ namespace Speeches
         {
             InitializeComponent();
         }
-        //warakoy lingaw
+        //Test
         //protected override void OnAppearing()
         //{
         //    base.OnAppearing();
         //    SplashAnimation.Progress = new TimeSpan(0, 0, 0);
         //    SplashAnimation.IsAnimationEnabled = true;
         //}
+
+        //Animation ends 
         private async void OnAnimationCompleted(object? sender, EventArgs e)
         {
             await Task.Delay(500);
+            //Next page
             await Navigation.PushAsync(new Login());
             Navigation.RemovePage(this);
         }
